a9d1567b026092a023f622f569e437e6f33cf0ef
soootaleb <talebs@icloud.com>
2022-06-29 19:32:09 +0200

    add defaults refresh button

--- ui/src/Defaults.js
+++ ui/src/Defaults.js
@@ -63,35 +63,48 @@ export default class Defaults extends React.Component {
     });
   }
 
+  refresh = async () => {
+    await this.props.client?.monget("/ddapps/node/state/defaults")
+      .then((response) => {
+        this.fetched = true
+        this.setState({ defaults: response.payload.payload.metric.value });
+        this.setState({ values: response.payload.payload.metric.value });
+      })
+  };
+
   render() {
     return (
-      <Accordion defaultActiveKey="0">
-        {
-          Object.keys(this.state.defaults).map((type) => {
-            return (
-              <Accordion.Item key={type} eventKey={type}>
-                <Accordion.Header>
-                  <h3>{type.substring(0, 1).toUpperCase() + type.substring(1)}</h3>
-                </Accordion.Header>
-                <Accordion.Body>
-                  {
-                    Object.keys(this.state.defaults[type]).map((param) => {
-                      return (
-                        <div key={type + param} id={type + param} className="param">
-                          <span className="paramlabel">{param}</span>
-                          <input className="paramvalue" onChange={(ev) => this.config(type, param, parseFloat(ev.target.value) || 0)} type="number" min="-1" max="1" step="0.1" value={this.state.values[type][param]._value || 0}></input>
-                          <Button onClick={() => this.commit(type, param)} >Commit</Button>
-                        </div>
-                      );
+      <>
+        <Button onClick={this.refresh} variant="primary">Refresh</Button>
+        <hr/>
+        <Accordion defaultActiveKey="0">
+          {
+            Object.keys(this.state.defaults).map((type) => {
+              return (
+                <Accordion.Item key={type} eventKey={type}>
+                  <Accordion.Header>
+                    <h3>{type.substring(0, 1).toUpperCase() + type.substring(1)}</h3>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    {
+                      Object.keys(this.state.defaults[type]).map((param) => {
+                        return (
+                          <div key={type + param} id={type + param} className="param">
+                            <span className="paramlabel">{param}</span>
+                            <input className="paramvalue" onChange={(ev) => this.config(type, param, parseFloat(ev.target.value) || 0)} type="number" min="-1" max="1" step="0.1" value={this.state.values[type][param]._value || 0}></input>
+                            <Button onClick={() => this.commit(type, param)} >Commit</Button>
+                          </div>
+                        );
 
-                    })
-                  }
-                </Accordion.Body>
-              </Accordion.Item>
-            );
-          })
-        }
-      </Accordion>
+                      })
+                    }
+                  </Accordion.Body>
+                </Accordion.Item>
+              );
+            })
+          }
+        </Accordion>
+      </>
     );
   }
 }
